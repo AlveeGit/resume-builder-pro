49837f204e3849f5646f2a1f3b97f889e7654244
AlveeGit <alveekabir1@gmail.com>
2026-06-13 23:36:07 +0600

    feat(resume): add ResumeRenderer template routing system

--- src/components/resume/ResumeRenderer.tsx
+++ src/components/resume/ResumeRenderer.tsx
@@ -1,2 +1,35 @@
-// TODO: implement ResumeRenderer.tsx
-export {};
+// ResumeRenderer.tsx
+"use client";
+
+import { ResumeData } from "@/types/resume";
+
+import { TemplateUK } from "./templates/TemplateUK";
+// future imports
+// import { TemplateBD } from "./templates/TemplateBD";
+// import { TemplateEU } from "./templates/TemplateEU";
+// import { TemplateInternational } from "./templates/TemplateInternational";
+
+export function ResumeRenderer({ resume }: { resume: ResumeData }) {
+  const { region, templateId } = resume;
+
+  // 🔥 fallback logic
+  const key = `${region}-${templateId}`;
+
+  switch (key) {
+    case "uk-default":
+    case "uk":
+      return <TemplateUK data={resume} />;
+
+    // case "bd-default":
+    //   return <TemplateBD data={resume} />;
+
+    // case "eu-default":
+    //   return <TemplateEU data={resume} />;
+
+    // case "international-default":
+    //   return <TemplateInternational data={resume} />;
+
+    default:
+      return <TemplateUK data={resume} />;
+  }
+}
--- src/components/resume/templates/TemplateUK.tsx
+++
@@ -1,2 +0,0 @@
-// TODO: implement TemplateUK.tsx
-export {};
