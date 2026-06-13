56ae32f32547475e7fc123e7d94f5382d55dc584
AlveeGit <alveekabir1@gmail.com>
2026-06-14 03:36:27 +0600

    feat(config): updated next, vercel, git action config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+
+  poweredByHeader: false,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "supabase.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
